5aa75aff480e4bbb5175c07374aa49dedc40f70f
Nguyen Quang <quangnn906@gmail.com>
2024-11-25 18:09:25 +0700

    Step 0: Create base project

---
+++ src/main/java/com/zett/quizzang/QuizzangApplication.java
@@ -0,0 +1,13 @@
+package com.zett.quizzang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zett/quizzang/QuizzangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zett.quizzang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
